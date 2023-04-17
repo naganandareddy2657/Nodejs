ed27de5ad79fb4dc29c7fc4180f21c8b4a40485b
naganandareddy2657 <naganandareddy2657@gmail.com>
2023-04-17 21:38:22 +0530

    hele

--- naga.js
+++ naga.js
@@ -24,7 +24,7 @@ function cbcommit(err, strout, sdtin){
         console.log(err);
         return;
     }
-   exec('git push -u origin master');
+   
 }
 
 //git clone https://github.com/naganandareddy2657/Nodejs.git
